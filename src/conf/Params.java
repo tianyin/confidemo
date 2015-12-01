9d674bf89dea6bc5c26b401a25bc03acb9d4ca98
Tianyin Xu <tixu@cs.ucsd.edu>
2015-11-30 16:21:53 -0800

    [INIT] refactoring the structure

--- src/conf/Params.java
+++ src/conf/Params.java
@@ -1,5 +1,9 @@
 package conf;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 import checks.FileChecker;
 import checks.TypeChecker;
 
@@ -9,7 +13,7 @@ public class Params {
   @ParamAttr(
     desc="Tachyon installation folder.", 
     scope = Scope.COMMON,
-    required=true,
+    mandatory=true,
     checks={TypeChecker.class}
   )
   public static final String TACHYON_HOME = "tachyon.home";
@@ -28,4 +32,18 @@ public class Params {
   )
   public static final String TACHYON_LOGGER_TYPE = "tachyon.logs.dir";
   
+  
+  
+  public static Map<String, ParamAttr> getParamAttrs() throws IllegalArgumentException, IllegalAccessException {
+    Map<String, ParamAttr> pmap = new HashMap<String, ParamAttr>();
+    Field[] fields = Params.class.getFields();
+    for(Field f : fields){
+      Object v = f.get(f.getType());
+      if (v instanceof String) {
+        ParamAttr p = f.getAnnotation(ParamAttr.class);
+        pmap.put((String) v, p);
+      }
+    }
+    return pmap;
+  }
 }
